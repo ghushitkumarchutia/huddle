23b9de0b1e52289588a15e39386afef25a1a6152
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-09-04 02:00:24 +0530

    feat(users): create thin controller layer wrapping user services in consistent response format

---
+++ server/src/modules/users/user.controller.js
@@ -0,0 +1,40 @@
+const userServices = require("./user.services");
+const asyncHandler = require("../../common/utils/asyncHandler");
+const ApiResponse = require("../../common/utils/apiResponse");
+
+const getProfile = asyncHandler(async (req, res) => {
+  const user = await userServices.getPublicProfile(req.params.userId);
+  res.status(200).json(new ApiResponse(200, user));
+});
+
+const updateProfile = asyncHandler(async (req, res) => {
+  const user = await userServices.updateProfile(req.user._id, req.body);
+  res
+    .status(200)
+    .json(new ApiResponse(200, user, "Profile updated successfully"));
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+  await userServices.changePassword(
+    req.user._id,
+    req.body.currentPassword,
+    req.body.newPassword,
+  );
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Password changed successfully"));
+});
+
+const deleteAccount = asyncHandler(async (req, res) => {
+  await userServices.deleteAccount(req.user._id);
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Account deleted successfully"));
+});
+
+module.exports = {
+  getProfile,
+  updateProfile,
+  changePassword,
+  deleteAccount,
+};
