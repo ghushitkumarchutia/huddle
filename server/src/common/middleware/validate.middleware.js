2da387971b1b6d27461103a1114c730656001754
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-09-03 22:34:04 +0530

    feat(middleware): implement generic zod schema validation middleware

---
+++ server/src/common/middleware/validate.middleware.js
@@ -0,0 +1,22 @@
+const ApiError = require('../utils/apiError');
+
+const validate = (schema) => {
+  return (req, res, next) => {
+    try {
+      if (schema.body) {
+        req.body = schema.body.parse(req.body);
+      }
+      if (schema.params) {
+        req.params = schema.params.parse(req.params);
+      }
+      if (schema.query) {
+        req.query = schema.query.parse(req.query);
+      }
+      next();
+    } catch (error) {
+      next(new ApiError(400, 'Validation failed', error.errors || error.message));
+    }
+  };
+};
+
+module.exports = validate;
