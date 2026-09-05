f8319af0b8649d715dee63ccd2e0b82d0706b3b0
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-09-06 00:05:53 +0530

    feat(follows): create thin controller to manage follow logic and lists

---
+++ server/src/modules/follows/follow.controller.js
@@ -0,0 +1,46 @@
+const followServices = require("./follow.services");
+const asyncHandler = require("../../common/utils/asyncHandler");
+const ApiResponse = require("../../common/utils/apiResponse");
+
+const follow = asyncHandler(async (req, res) => {
+  const { spaceId, userId: followingId } = req.params;
+  const followDocument = await followServices.followUser(
+    req.user.id,
+    followingId,
+    spaceId,
+  );
+  res
+    .status(201)
+    .json(new ApiResponse(201, followDocument, "Successfully followed user"));
+});
+
+const unfollow = asyncHandler(async (req, res) => {
+  const { spaceId, userId: followingId } = req.params;
+  await followServices.unfollowUser(req.user.id, followingId, spaceId);
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Successfully unfollowed user"));
+});
+
+const listFollowers = asyncHandler(async (req, res) => {
+  const { spaceId, userId } = req.params;
+  const followers = await followServices.listFollowers(userId, spaceId);
+  res
+    .status(200)
+    .json(new ApiResponse(200, followers, "Followers retrieved successfully"));
+});
+
+const listFollowing = asyncHandler(async (req, res) => {
+  const { spaceId, userId } = req.params;
+  const following = await followServices.listFollowing(userId, spaceId);
+  res
+    .status(200)
+    .json(new ApiResponse(200, following, "Following retrieved successfully"));
+});
+
+module.exports = {
+  follow,
+  unfollow,
+  listFollowers,
+  listFollowing,
+};
